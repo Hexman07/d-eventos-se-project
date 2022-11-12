7d12908e03cd321094d90e6829eca9a52b4d2b45
HimshikharSingh <himdothat13@gmail.com>
2022-11-12 23:27:49 +0530

    Added the middleware for the authentication and also for the demo user

---
+++ pages/api/middleware/auth.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken");
+require('dotenv').config()
+
+module.exports = function (req, res, next) {
+    const authHeader = req.header("Authorization").split(" ");
+    const token = authHeader[1];
+    // const token = req.cookies.jwt;
+    if (!token) return res.status(401).json({ message: "Auth Error" });
+
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded.user;
+        next();
+    } catch (e) {
+        console.error(e);
+        res.status(400).send({ message: "Invalid Token", error: e });
+    }
+};
